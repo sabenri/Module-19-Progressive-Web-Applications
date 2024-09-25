cea31c87dec71542c37de0bbdfe7f9473efa1f60
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-24 20:24:29 -0400

    finished up the database js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate',1);
+  const tx =db.transaction('jate','readwrite');
+  const store = tx.objectStore('jate');
+  const result = await store.put({ id:1, content});
+  await tx.done;
+  console.log('Date Saved To The Database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate',1);
+  const tx = db.transaction('jate','readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.get(1);
+  await tx.done;
+  return result?.content; 
+};
 
 initdb();
